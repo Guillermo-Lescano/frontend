94ff76242a6cd35b27ec7335882d349cffe38fe8
Guillermo Lescano <guillelescano.gl@gmail.com>
2023-10-12 01:59:19 -0300

    Creacion de componentes Creando rutas con react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-function App() {
+import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import AuthLayout from './layouts/AuthLayout'
+import Login from './paginas/Login'
+import Registrar from './paginas/Registrar'
+import OlvidePassword from './paginas/OlvidePassword'
+import NuevoPassword from './paginas/NuevoPassword'
+import ConfirmarCuenta from './paginas/ConfirmarCuenta'
 
+const App = () => {
   return (
-    <>
-      <div className="card">
-        <h1>Uptask</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AuthLayout />}>
+          <Route index element={<Login />}/>
+          <Route path='registrar' element={<Registrar />}/>
+          <Route path='olvide-password' element={<OlvidePassword />}/>
+          <Route path='olvide-password/:token' element={<NuevoPassword />}/>
+          <Route path='confirmar-cuenta/:id' element={<ConfirmarCuenta />}/>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
