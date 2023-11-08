4d2f232b5aeb2103b711fb4fa6eb586beb036509
Guillermo Lescano <guillelescano.gl@gmail.com>
2023-11-08 20:24:38 -0300

    Recuperando Pass primera parte

--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,13 +1,40 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Alert from "../components/Alert";
 
 const OlvidePassword = () => {
+  const [email, setEmail] = useState("");
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (email === " " || email.length < 6) {
+      setAlerta({
+        msg: "El email es obligatorio",
+        error: true
+      });
+      return;
+    }
+
+    //generamos email para la recuperacion
+
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-5xl capitalize">
         Recupera tu acceso y no pierdas tus{" "}
         <span className="text-slate-700">proyectos</span>
       </h1>
-      <form action="" className="my-10 bg-white shadow rounded-lg p-10">
+      {msg && <Alert alerta={alerta} />}
+      <form
+        onSubmit={handleSubmit}
+        action=""
+        className="my-10 bg-white shadow rounded-lg p-10"
+      >
         <div className="m-5">
           <label
             htmlFor="email"
@@ -20,6 +47,8 @@ const OlvidePassword = () => {
             type="email"
             placeholder="Email de Registro"
             className="w-full mt-3 p-2 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <input
@@ -38,12 +67,14 @@ const OlvidePassword = () => {
           ¿Ya tienes una cuenta? Inicia Sesion
         </Link>
         <Link
-        className="flex text-center my-5 text-slate-500 uppercase text-sm"
-          to='registrar'
-        >¿No tienes una cuenta? Regístrate</Link>
+          className="flex text-center my-5 text-slate-500 uppercase text-sm"
+          to="registrar"
+        >
+          ¿No tienes una cuenta? Regístrate
+        </Link>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default OlvidePassword
+export default OlvidePassword;
