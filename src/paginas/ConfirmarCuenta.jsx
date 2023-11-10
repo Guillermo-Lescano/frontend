0b62f63f12f023d37b4a90f5bf7a256bc22f676f
Guillermo Lescano <guillelescano.gl@gmail.com>
2023-11-10 14:25:29 -0300

    Validando autenticacion

--- src/paginas/ConfirmarCuenta.jsx
+++ src/paginas/ConfirmarCuenta.jsx
@@ -16,16 +16,20 @@ const ConfirmarCuenta = () => {
     const confirmarCuenta = async () =>{
       try {
         const url = `/usuarios/confirmar/${id}`
-        const {data} = await clienteAxios(url)
-
+        const { data } = await clienteAxios(url)
+        console.log('data', data)
         setAlerta({
           msg: data.msg,
           error: false
         })
+        
         setCuentaConfirmada(true)
 
       } catch (error) {
-        setAlerta({msg: error.response.data.msg, error: true})
+        console.log(error.response.data.msg)
+        setAlerta({
+          msg: error.response.data.msg, 
+          error: true})
       }
 
     }
@@ -37,7 +41,7 @@ const ConfirmarCuenta = () => {
   return (
     <>
       <h1 className="text-sky-600 font-black text-5xl capitalize">
-        Confirma tu cuenta y comienza a crear tus proyectos{" "}
+        Confirma tu cuenta y comienza a crear tus{" "}
         <span className="text-slate-700">proyectos</span>
       </h1>
 
--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,13 +1,43 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Alert from "../components/Alert";
+import clienteAxios from "../config/clienteAxios";
 
 const Login = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [alerta, setAlerta] = useState({})
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+
+    if([email, password].includes('')){
+      setAlerta({
+        msg: 'Todos los campos son requeridos',
+        error: true
+      })
+      return
+    }
+
+    try {
+      
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  const {msg} = alerta
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-5xl capitalize">
         Inicia sesión y administra tus{" "}
         <span className="text-slate-700">proyectos</span>
       </h1>
-      <form action="" className="my-10 bg-white shadow rounded-lg p-10">
+      {msg && <Alert alerta={alerta} />}
+      <form onSubmit={handleSubmit} className="my-10 bg-white shadow rounded-lg p-10">
         <div className="m-5">
           <label
             htmlFor="email"
@@ -20,6 +50,8 @@ const Login = () => {
             type="email"
             placeholder="Email de Registro"
             className="w-full mt-3 p-2 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
         <div className="m-5">
@@ -34,6 +66,8 @@ const Login = () => {
             type="password"
             placeholder="Password de Registro"
             className="w-full mt-3 p-2 border rounded-xl bg-gray-50"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
         </div>
 
