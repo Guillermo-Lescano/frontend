c2c893849091d1eb20800e97062cf142e8e7b996
Guillermo Lescano <guillelescano.gl@gmail.com>
2023-11-13 00:13:18 -0300

    Creando el Contex para Proyectos

--- src/App.jsx
+++ src/App.jsx
@@ -13,25 +13,28 @@ import Proyectos from "./paginas/Proyectos";
 import NuevoProyecto from "./paginas/NuevoProyecto";
 
 import { AuthProvider } from "./context/AuthProvider";
+import { ProyectosProvider } from "./context/ProyectosProvider";
 
 const App = () => {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AuthLayout />}>
-            <Route index element={<Login />} />
-            <Route path="registrar" element={<Registrar />} />
-            <Route path="olvide-password" element={<OlvidePassword />} />
-            <Route path="olvide-password/:token" element={<NuevoPassword />} />
-            <Route path="confirmar-cuenta/:id" element={<ConfirmarCuenta />} />
-          </Route>
-          //Area Privada
-          <Route path="/proyectos" element={<RutaProtegida />}> //este componente lo que hace es tene rla logica para proteger a los proximos componentes
-            <Route index element={<Proyectos />} />
-            <Route path='crear-proyecto' element={<NuevoProyecto />} />
-          </Route>
-        </Routes>
+        <ProyectosProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Login />} />
+              <Route path="registrar" element={<Registrar />} />
+              <Route path="olvide-password" element={<OlvidePassword />} />
+              <Route path="olvide-password/:token" element={<NuevoPassword />} />
+              <Route path="confirmar-cuenta/:id" element={<ConfirmarCuenta />} />
+            </Route>
+            //Area Privada
+            <Route path="/proyectos" element={<RutaProtegida />}> //este componente lo que hace es tene rla logica para proteger a los proximos componentes
+              <Route index element={<Proyectos />} />
+              <Route path='crear-proyecto' element={<NuevoProyecto />} />
+            </Route>
+          </Routes>
+        </ProyectosProvider>
       </AuthProvider>
     </BrowserRouter>
   );
