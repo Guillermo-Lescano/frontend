323ab7d005bd796f80bfa822b5a14cc25cd6fedf
Guillermo Lescano <guillelescano.gl@gmail.com>
2023-11-13 00:23:03 -0300

    Creando el Hook para el Context de Proyectos

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -4,10 +4,13 @@ import clienteAxios from "../config/clienteAxios";
 const ProyectosContext = createContext();
 
 const ProyectosProvider = ({ children }) => {
+
+  const [proyectos, setProyectos] = useState([])
+
   return (
     <ProyectosContext.Provider 
     value={{
-
+      proyectos
     }}
     >
         {children}
