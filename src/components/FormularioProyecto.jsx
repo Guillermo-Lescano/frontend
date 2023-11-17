2c8fc7f8ca80f6e4be01653f98bdfd520ea642b0
Guillermo Lescano <guillelescano.gl@gmail.com>
2023-11-17 01:12:22 -0300

    Llenando el formulario cuando queremos editar

--- src/components/FormularioProyecto.jsx
+++ src/components/FormularioProyecto.jsx
@@ -1,39 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import useProyectos from "../hooks/useProyectos";
 import Alert from "./Alert";
 
 const FormularioProyecto = () => {
+  const [id, setId] = useState(null);
   const [nombre, setNombre] = useState("");
   const [descripcion, setDescripcion] = useState("");
   const [fechaEntrega, setFechaEntrega] = useState("");
   const [cliente, setCliente] = useState("");
 
-  const {mostrarAlerta, alerta, submitProyecto} = useProyectos()
+  //este params nos va a servir para saber si estamos editando o no, si tiene algo el params estamos editando, y si no tiene , creando
+  const params = useParams();
 
-  const handleSubmit = async(e) => {
-    e.preventDefault()
+  const { mostrarAlerta, alerta, submitProyecto, proyecto } = useProyectos();
 
-    if([nombre, descripcion, fechaEntrega, cliente].includes('')){
+  useEffect(() => {
+    if(params.id){
+      setId(proyecto._id)
+      setNombre(proyecto.nombre)
+      setDescripcion(proyecto.descripcion)
+      setFechaEntrega(proyecto.fechaEntrega?.split('T')[0])
+      setCliente(proyecto.cliente)
+    } else {
+
+    }
+  },[params])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if ([nombre, descripcion, fechaEntrega, cliente].includes("")) {
       mostrarAlerta({
-        msg:'Todos los campos son obligatorios',
-        error: true
-      })
-      return
+        msg: "Todos los campos son obligatorios",
+        error: true,
+      });
+      return;
     }
     //Pasar los datos al Provider
-    await submitProyecto({nombre, descripcion, fechaEntrega, cliente})
-    setNombre('')
-    setDescripcion('')
-    setFechaEntrega('')
-    setCliente('')
-  }
+    await submitProyecto({ nombre, descripcion, fechaEntrega, cliente });
+    setNombre("");
+    setDescripcion("");
+    setFechaEntrega("");
+    setCliente("");
+  };
 
-  const { msg } = alerta
+  const { msg } = alerta;
 
   return (
-    
-    <form onSubmit={handleSubmit} className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow">
-      {msg && <Alert alerta={alerta}/>}
+    <form
+      onSubmit={handleSubmit}
+      className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow"
+    >
+      {msg && <Alert alerta={alerta} />}
       <div className="mb-5">
         <label
           htmlFor="nombre"
@@ -98,7 +117,7 @@ const FormularioProyecto = () => {
         />
         <input
           type="submit"
-          value="Crear proyecto"
+          value={id ? 'Actualizar' : 'Crear Proyecto'}
           className="bg-sky-600 w-full p-3 my-3 uppercase font-bold text-white rounded cursor-pointer hover:bg-sky-700 transition-colors"
         />
       </div>
